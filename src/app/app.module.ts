39d77523c5c73b725289127aaead5809a3ab974c
KvasRoman <romankhmil0705@gmail.com>
2023-07-13 17:21:25 +0300

    add home page with discover and browse the range frames

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './main/defaultUser/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
